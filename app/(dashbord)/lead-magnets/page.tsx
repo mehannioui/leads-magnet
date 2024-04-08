781262b0590c529632aedd35b11fee73d952666c
Mohamed Hannioui <mehannioui@gmail.comm>
2024-04-08 01:54:36 -0400

    add subscription

--- app/(dashbord)/lead-magnets/page.tsx
+++ app/(dashbord)/lead-magnets/page.tsx
@@ -8,6 +8,7 @@ const getLeadMagnets = async (userId: string) => {
     const leadMagnets = await prismadb.leadMagnet.findMany({
       where: { userId },
     });
+
     return leadMagnets;
   } catch (error) {
     console.error(error);
@@ -20,6 +21,7 @@ const getLeads = async (userId: string) => {
     const leads = await prismadb.lead.findMany({
       where: { userId },
     });
+
     return leads;
   } catch (error) {
     console.error(error);
@@ -27,22 +29,46 @@ const getLeads = async (userId: string) => {
   }
 };
 
+const getSubscription = async (userId: string) => {
+  try {
+    const subscription = await prismadb.subscription.findUnique({
+      where: { userId },
+    });
+
+    return subscription;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
 async function LeadMagnetsPage() {
   const { userId } = auth();
+
+  console.log('userId', userId);
+
   if (!userId) return <div>No user found...</div>;
 
   const leadMagnetsRequest = getLeadMagnets(userId);
   const leadsRequest = getLeads(userId);
+  const subscriptionRequest = getSubscription(userId);
 
-  const [leadMagnets, leads] = await Promise.all([
+  const [leadMagnets, leads, subscription] = await Promise.all([
     leadMagnetsRequest,
     leadsRequest,
+    subscriptionRequest,
   ]);
 
   console.log('leadMagnets', leadMagnets);
   console.log('leads', leads);
 
-  return <LeadMagnetsContainer leadMagnets={leadMagnets} leads={leads} />;
+  return (
+    <LeadMagnetsContainer
+      leadMagnets={leadMagnets}
+      leads={leads}
+      subscription={subscription}
+    />
+  );
 }
 
 export default LeadMagnetsPage;
